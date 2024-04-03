ec5331ac7404fa968b5cd6c44f7993f99b631486
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-03 16:03:49 -0400

    began implementing the icon-button

---
+++ app/components/icon-button.jsx
@@ -0,0 +1,10 @@
+import React, {} from 'react';
+import {createUseStyles} from 'react-jss';
+
+export default function IconButton({icon}) {
+    return (
+        <div>
+            <i>{icon}</i>
+        </div>
+    );
+}
