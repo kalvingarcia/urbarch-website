8c5ddefc81431337462a747a9036fbc5ef07bc68
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-08 15:58:17 -0400

    begun migrating theme

--- app/components/theme.jsx
+++ app/components/theme.jsx
@@ -21,6 +21,7 @@ const lion = "##AB8F5C"; // Secondary Dark Color
 const clot = "#C2AEAE"; // Error Light Color
 const wine = "#4F1E2B"; // Error Dark Color
 
+// The theme on Light Mode
 const lightTheme = {
     title: prussian_blue,
     subtitle: flax,
@@ -43,45 +44,50 @@ const lightTheme = {
     onError: clot
 };
 
-const darkTheme = {
-    title: alice_blue,
-    subtitle: flax,
-    heading: air_force,
-    subheading: alabaster,
-    body: sea_salt,
+// // The theme on Dark Mode
+// const darkTheme = {
+//     title: alice_blue,
+//     subtitle: flax,
+//     heading: air_force,
+//     subheading: alabaster,
+//     body: sea_salt,
 
-    lightFont: sea_salt,
-    darkFont: raisin_black,
+//     lightFont: sea_salt,
+//     darkFont: raisin_black,
 
-    background: raisin_black,
-    surface: onyx,
+//     background: raisin_black,
+//     surface: onyx,
 
-    primary: prussian_blue,
-    onPrimary: alice_blue,
-    secondary: lion,
-    onSecondary: alabaster,
+//     primary: prussian_blue,
+//     onPrimary: alice_blue,
+//     secondary: lion,
+//     onSecondary: alabaster,
 
-    error: clot,
-    onError: wine
-};
+//     error: clot,
+//     onError: wine
+// };
 
 export default function Theme({children}) {
-    const [theme, setTheme] = useState(lightTheme);
-    const [darkMode, setDarkMode] = useState(false);
+    const [theme, setTheme] = useState(lightTheme); // We set the default theme to light mode
+    
+    const [darkMode, setDarkMode] = useState(false); // This keeps track of dark mode
+    // The function to toggle dark mode
     const toggleDarkMode = useCallback(() => {
-        if(darkMode) {
-            setTheme(lightTheme);
-            setDarkMode(false);
-        } else {
-            setTheme(darkTheme);
-            setDarkMode(true);
-        }
-    }, [darkMode]);
+        if(darkMode) // If in dark mode
+            setDarkMode(false); // Become light mode
+        else // If light mode
+            setDarkMode(true); // Become dark mode
+    }, [darkMode]); // Here we will optain 2 cached functions
 
+    // We send darkMode and toggleDarkMode so that any component can access the darkMode information and change it
     return (
-        <DarkModeContext.Provider value={[darkMode, toggleDarkMode]} >
+        <DarkModeContext.Provider value={[darkMode, toggleDarkMode]}>
             <ThemeProvider theme={theme}>
-                {children}
+                <div className={darkMode? 'theme--dark' : 'theme--light'}>
+                    <div className="body">
+                        {children}
+                    </div>
+                </div>
             </ThemeProvider>
         </DarkModeContext.Provider>
     );
