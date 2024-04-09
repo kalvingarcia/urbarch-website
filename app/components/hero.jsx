708da05a290f893e6e652eac491903a9d387bb57
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-09 15:04:37 -0400

    migrated hero from css-in-js

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -1,55 +1,38 @@
+"use client";
 import Image from 'next/image';
-import React from 'react';
-import {createUseStyles} from 'react-jss';
+import  {useCallback, useEffect, useState} from 'react';
+import IconButton from './icon-button';
 import background from '../assets/backgrounds/home.png';
+import '../assets/styles/components/hero.scss';
 
-const useStyles = createUseStyles({
-    hero: {
-        width: "100%",
-        height: "100vh",
-        position: "relative"
-    },
-    parallaxContainer: {
-        height: "100%",
-        overflow: "hidden",
-        clipPath: "inset(0 0 0 0)"
-    },
-    parallaxImage: {
-        height: "100%",
-        width: "auto",
-        objectFit: "cover",
-        objectPosition: "center",
-        position: "fixed"
-    },
-    gradient: {
-        backgroundImage: "linear-gradient(0deg, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0) 100%)",
-        height: "100%",
-        width: "100%",
-        position: "absolute",
-        top: "0",
-        left: "0"
-    },
-    heading: {
-        maxWidth: "1280px",
-        position: "absolute",
-        bottom: "0",
-        marginLeft: "max(calc(max(100vw - 1280px, 0px) / 2), 25px)",
-        marginBottom: "100px",
+export default function Hero({src, children}) {
+    // Here we have a callback for showing the hero content on mouse move
+    const [hide, setHide] = useState(true);
+    const [timer, setTimer] = useState(undefined);
+    const timedHide = useCallback(() => {
+        setHide(false); // We unhide the hero's content
+        if(timer) // If there is a timer already
+            clearTimeout(timer); // We stop it
 
-        color: "white",
-        fontSize: "100px",
-    }
-});
+        // Here we (re)set a timer for 5 seconds
+        setTimer(setTimeout(() => {
+            setHide(true);
+            setTimer(undefined); // Set Timer to undefined by default
+        }, 5000));
+    }, []);
 
-export default function Hero() {
-    const styles = useStyles();
     return (
-        <section className={styles.hero}>
-            <figure className={styles.parallaxContainer}>
-                <Image className={styles.parallaxImage} src={background} alt="Hompage background image" priority/>
+        <section className={['hero', hide? "hide" : ""].join(" ")} onMouseMove={timedHide}>
+            <figure className='parallax-container'>
+                <Image className='parallax-image' src={background} alt="Hompage background image" priority/>
             </figure>
-            <div className={styles.gradient} />
-            <div className={styles.heading}>Knobs are really cool!</div>
+            <div className='gradient' />
+            <div className='content'>
+                {children}
+            </div>
+            <div className='visibility'>
+                <IconButton role="secondary" style="outlined" icon={hide? "visibility" : "visibility_off"} onPress={() => setHide(!hide)}/>
+            </div>
         </section>
     );
 }
