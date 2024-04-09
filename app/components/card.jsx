3a4e29387d1f08524dfd3f64d851bb0ef87c268e
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-09 15:06:52 -0400

    begun developing the card component

---
+++ app/components/card.jsx
@@ -0,0 +1,34 @@
+"use client"
+import {useEffect, useState} from 'react';
+import useRippleEffect from '../hooks/ripple';
+import '../assets/styles/components/card.scss';
+import Image from 'next/image';
+
+export default function Card({type = "normal", name, category, price, uaid}) {
+    const [loading, setLoading] = useState(true);
+    const [image, setImage] = useState("");
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+            setImage(await import(`../assets/products/${uaid}.jpg`));
+            setLoading(false);
+        })();
+    }, [uaid]);
+
+    const [rippleExpand, rippleFade] = useRippleEffect();
+    return (
+        <div className={['card', type].join(" ")} onMouseDown={rippleExpand} onMouseUp={rippleFade}>
+            <div className='image'>
+                <Image src={image} alt="" />
+            </div>
+            <div className='content'>
+                <div className='metadata'>
+                    <span className='name'>{name}</span>
+                    <span className='categrpy'>{category}</span>
+                    <span className='price'>{price}</span>
+                </div>
+                <span className='uaid'>{uaid}</span>
+            </div>
+        </div>
+    );
+}
