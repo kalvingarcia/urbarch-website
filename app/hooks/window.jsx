54f7ab5f392f708bea7cd69e8db0c4296ae0b369
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-10 12:30:00 -0400

    added a use window size hook

---
+++ app/hooks/window.jsx
@@ -0,0 +1,26 @@
+"use client"
+import {useEffect, useState} from 'react';
+
+export default function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+        width: 0,
+        height: 0
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
+        }
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowSize;
+}
