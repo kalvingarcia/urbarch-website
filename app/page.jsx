b1338539dc104354f8803138ece340b41e1ef213
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-15 15:59:30 -0400

    added skeleton loading to cards

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,10 @@
-"use client"
-import {useEffect, useState} from 'react';
-import Hero from './components/hero';
-import Display from './components/display';
-import Button from './components/button';
-import Featured from './components/featured';
-import Card from './components/card';
-import Banner from './components/banner';
-import useWindowSize from './hooks/window';
+import Hero from './assets/components/hero';
+import {Display} from './assets/components/typography';
+import Button from './assets/components/button';
+import Featured from './assets/components/featured';
+import Banner from './assets/components/banner';
 
 export default function Home() {
-    const [view, setView] = useState("normal");
-    const {width} = useWindowSize();
-    useEffect(() => {
-        setView("normal");
-        if(width < 1000)
-            setView("list");
-    }, [width]);
-
     return (
         <main>
             <Hero src="home.jpg">
@@ -26,11 +14,7 @@ export default function Home() {
                 <Display size="small">@ 7:00 AM - 4:00 PM</Display>
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdL1pVylvGkvPjGTv31c1-C0y_04Xfp3KzbrWaPNWAe2u-SJg/viewform"><Button role="secondary" style="outlined">RSVP Here!</Button></a>
             </Hero>
-            <Featured>
-                <Card type={view} name="Banded Beacon [Small]" category="Lighting" price="$300" uaid="UA0000" />
-                <Card type={view} name="Banded Beacon [Small]" category="Lighting" price="$300" uaid="UA0000" />
-                <Card type={view} name="Banded Beacon [Small]" category="Lighting" price="$300" uaid="UA0000" />
-            </Featured>
+            <Featured />
             <Banner src="custom.png">
                 <Display size="small">Making dreams come true!</Display>
                 <span>If you do not see what you are looking for on this website, we are able to customize new pieces to meet your specifications.</span>
