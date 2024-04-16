bd90795e9ed567781fad609050dd7f80006f17c2
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-16 16:26:41 -0400

    added suspense to featured instead of loading state

--- app/assets/components/featured.jsx
+++ app/assets/components/featured.jsx
@@ -1,41 +1,53 @@
 "use client"
-import {useEffect, useState} from 'react';
+import {Suspense, useEffect, useState} from 'react';
 import {Title} from "./typography";
 import Card, {CardSkeleton} from './card';
 import useWindowSize from '../hooks/window';
 import {GET_FEATURED_PRODUCTS} from '../../api';
 import "../styles/components/featured.scss";
 
-export default function Featured() {
-    const [view, setView] = useState("normal");
-    const {width} = useWindowSize();
-    useEffect(() => {
-        setView("normal");
-        if(width < 1000)
-            setView("list");
-    }, [width]);
-
-    const [loading, setLoading] = useState(true);
-    const [featured, setFeatured] = useState([]);
-    useEffect(() => {
-        (async () => {
-            setFeatured(await fetch(GET_FEATURED_PRODUCTS).then(response => response.json()));
-            setLoading(false);
-        })();
-    }, []);
-
+export function FeaturedSkeleton({view}) {
     return (
         <section className="featured">
             <Title className="title">Featured Products</Title>
             <div className="divider" />
             <div className="cards">
-                {loading?
-                    [<CardSkeleton type={view} />, <CardSkeleton type={view} />, <CardSkeleton type={view} />] : 
-                    featured.map(data => (
-                        <Card type={view} uaid={data.id} name={data.name} category={data.category} />
-                    ))
-                }
+                <CardSkeleton type={view} />
+                <CardSkeleton type={view} />
+                <CardSkeleton type={view} />
             </div>
         </section>
+    );
+}
+
+async function AsyncFeatured({view}) {
+    const featured = await fetch(GET_FEATURED_PRODUCTS).then(response => response.json());
+    return (
+        <section className="featured">
+            <Title className="title">Featured Products</Title>
+            <div className="divider" />
+            <div className="cards">
+                {featured.map(data => (
+                    <Card key={data.id} type={view} uaid={data.id} name={data.name} category={data.category} price={data.price} />
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default function Featured() {
+    const [view, setView] = useState("normal");
+    const {width} = useWindowSize();
+    useEffect(() => {
+        if(width < 1000 && view === "normal")
+            setView("list");
+        else if(width >= 1000 && view === "list")
+            setView("normal");
+    }, [width]);
+
+    return (
+        <Suspense fallback={<FeaturedSkeleton view={view} />}>
+            <AsyncFeatured view={view} />
+        </Suspense>
     );
 }
