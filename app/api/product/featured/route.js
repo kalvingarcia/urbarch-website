f6f52b6e772dbdae0baa0eb0dc7b7d3ae3366928
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-16 16:25:55 -0400

    implemented the get featured route in the api

---
+++ app/api/product/featured/route.js
@@ -0,0 +1,26 @@
+import Database from "../../database";
+
+export async function GET(request) {
+    const result = await Database`
+        WITH variations AS (
+                /* Create a table with the price and listing id for the variation */
+                SELECT listing_id, price FROM product_variations WHERE featured = TRUE
+            ), categories AS (
+                /* Create a table with the tag name and listing id */
+                SELECT listing_id, tag.name AS category
+                FROM tag INNER JOIN tag_categories ON tag.category_id = tag_categories.id  /* First we combine the tag and tag category information */
+                    INNER JOIN product_variation__tag ON product_variation__tag.tag_id = tag.id /* Then we combine the tags specific to the variations we have */
+                WHERE tag_categories.name = 'Category' AND listing_id IN (SELECT listing_id FROM variations)
+            ), product AS (
+                /* Create a table with the listing_id and name of the product */
+                SELECT id AS listing_id, name FROM product_listing WHERE id IN (SELECT listing_id FROM variations)
+            )
+        /* Finally we select from these combined tables to get the information to display on the card */
+        SELECT DISTINCT listing_id AS id, name, category, price
+        FROM variations INNER JOIN categories USING(listing_id) INNER JOIN product USING(listing_id);
+    `
+
+    return new Response(JSON.stringify(result), {
+        "status": 200
+    })
+}
