aec6e2b560d0cfb4dc3ec5b44a7f1e3da816bc5f
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-16 16:26:24 -0400

    modified the card to function using suspense instead of loading state

--- app/assets/components/card.jsx
+++ app/assets/components/card.jsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from 'next/image';
-import {useEffect, useState} from 'react';
+import {Suspense, useEffect, useState} from 'react';
 import useRippleEffect from '../hooks/ripple';
 import '../styles/components/card.scss';
 
@@ -20,36 +20,30 @@ export function CardSkeleton({type = "normal"}) {
     );
 }
 
-export default function Card({type = "normal", name, category, price, uaid}) {
-    const [loading, setLoading] = useState(true);
-    const [image, setImage] = useState("");
-    useEffect(() => {
-        (async () => {
-            setLoading(true);
-            setImage(await import(`../images/products/${uaid}.jpg`));
-            setLoading(false);
-        })();
-    }, [uaid]);
+async function AsyncCard({type = "normal", name, category, price, uaid, rippleExpand, rippleFade}) {
+    const image = await import(`../images/products/${uaid}.jpg`);
+    return (
+        <div className={['card', type].join(" ")} onMouseDown={rippleExpand} onMouseUp={rippleFade}>
+            <div className='image'>
+                <Image src={image} alt="" />
+            </div>
+            <div className='content'>
+                <div className='metadata'>
+                    <span className='name'>{name}</span>
+                    <span className='category'>{category}</span>
+                    <span className='price'>${price}</span>
+                </div>
+                <span className='uaid'>{uaid}</span>
+            </div>
+        </div>
+    );
+}
 
+export default function Card({...props}) {
     const [rippleExpand, rippleFade] = useRippleEffect();
-    const [card, setCard] = useState(<CardSkeleton />);
-    useEffect(() => {
-        setCard(<CardSkeleton />);
-        if(!loading)
-            setCard(<div className={['card', type].join(" ")} onMouseDown={rippleExpand} onMouseUp={rippleFade}>
-                <div className='image'>
-                    <Image src={image} alt="" />
-                </div>
-                <div className='content'>
-                    <div className='metadata'>
-                        <span className='name'>{name}</span>
-                        <span className='category'>{category}</span>
-                        <span className='price'>{price}</span>
-                    </div>
-                    <span className='uaid'>{uaid}</span>
-                </div>
-            </div>);
-    }, [loading]);
-
-    return card;
+    return (
+        <Suspense fallback={<CardSkeleton type={props.type} />}>
+            <AsyncCard {...props} rippleExpand={rippleExpand} rippleFade={rippleFade} />
+        </Suspense>
+    );
 }
