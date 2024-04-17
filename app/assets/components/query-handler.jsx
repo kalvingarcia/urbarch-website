d9019ab93edf1181f25d03b1ecbd77a42493d61a
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-17 13:25:01 -0400

    opted to abstract the query handling to its own component

---
+++ app/assets/components/query-handler.jsx
@@ -0,0 +1,60 @@
+"use client"
+import {useRouter, usePathname, useSearchParams} from 'next/navigation';
+import {createContext, useCallback, useState, useEffect} from "react";
+
+export const QueryContext = createContext();
+
+export default function QueryHandler({children}) {
+    const searchParameters = useSearchParams();
+    const [queryParameters, setQueryParameter] = useState({search: "", filters: {}});
+    useEffect(() => {
+        for(const [parameter, value] of searchParameters.entries())
+            if(parameter === "search")
+                setSearch(value);
+            else for(id of value.replace("(", "").replace(")", "").split("|"))
+                addFilter(parameter, id);
+    }, [searchParameters]);
+
+    const addFilter = useCallback((filterCategory, filterID) => {
+        const {filters} = queryParameters;
+        if(!filters.hasOwnProperty(filterCategory))
+            filters[filterCategory] = []
+        filters[filterCategory].push(filterID);
+        setQueryParameter({...queryParameters, filters: filters});
+    }, [queryParameters]);
+
+    const removeFilter = useCallback(filterID => {
+        const {filters} = queryParameters;
+        delete filters[filterID];
+        setQueryParameter({...queryParameters, filters: filters});
+    }, [queryParameters]);
+
+    const hasFilter = useCallback((filterCategory, filterID) => {
+        const {filters} = queryParameters;
+        return filters.hasOwnProperty(fifilterCategorylterID) && filters[filterCategory].includes(filterID);
+    }, [queryParameters]);
+
+    const setSearch = useCallback(searchText => {
+        setQueryParameter({...queryParameters, search: searchText})
+    }, [queryParameters]);
+
+    const pathname = usePathname();
+    const router = useRouter();
+    useEffect(() => {
+        const queryStringList = [];
+        if(queryParameters.hasOwnProperty("search") && queryParameters.search !== "")
+            queryStringList.push(`search=${queryParameters.search}`)
+        if(queryParameters.hasOwnProperty("filters") && Object.keys(queryParameters.filters).length !== 0)
+            for(const [key, value] of Object.entries(queryParameters.filters))
+                queryStringList.push(`${key}=${value}`);
+        const queryString = queryStringList.join("&").replace("\"", "").replace("[", "(").replace("]", ")").replace(",", "+");
+        if(queryString !== "")
+            router.push(`${pathname}?${queryString}`);
+    }, [queryParameters]);
+
+    return (
+        <QueryContext.Provider value={{addFilter, removeFilter, hasFilter, setSearch}} >
+            {children}
+        </QueryContext.Provider>
+    );
+}
--- app/catalog/page.jsx
+++ app/catalog/page.jsx
@@ -1,14 +1,28 @@
-import Banner from "../assets/components/banner"
-import {Display} from "../assets/components/typography"
-import CatalogBody from "../assets/components/catalog-body"
+import Banner from "../assets/components/banner";
+import {Display} from "../assets/components/typography";
+import QueryHandler from "../assets/components/query-handler";
+import Filters from "../assets/components/filters";
+// import Search from "../assets/components/search";
+import Listings from "../assets/components/listings";
+import {GET_PRODUCTS, GET_TAGS} from "../api";
 
-export default function Catalog() {
+export default function Catalog({searchParams}) {
+    const filters = fetch(GET_TAGS);
+    // const listings = fetch(`${GET_PRODUCTS}?${searchParams.toString()}`);
     return (
         <main>
             <Banner src="catalog.jpg">
                 <Display size="medium">Catalog</Display>
             </Banner>
-            <CatalogBody />
+            <QueryHandler>
+                <div>
+                    <Filters filterPromise={filters}/>
+                    <div>
+                        {/* <Search /> */}
+                        {/* <Listings listingsPromise={listings} /> */}
+                    </div>
+                </div>
+            </QueryHandler>
         </main>
     )
 }
