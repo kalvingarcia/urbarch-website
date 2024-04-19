7cdbb9d01231de791b59a83a2c70bc5fff8b7acf
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-19 16:11:48 -0400

    corrected redundancy

--- app/assets/components/icon-button.jsx
+++ app/assets/components/icon-button.jsx
@@ -1,9 +1,9 @@
 import Button from './button';
 import '../styles/components/icon-button.scss';
 
-export default function IconButton({className, icon, onPress, ...props}) {
+export default function IconButton({className, icon, ...props}) {
     return (
-        <Button className={["icon-button", className? className : ""].join(" ")} onPress={onPress} {...props}>
+        <Button className={["icon-button", className? className : ""].join(" ")} {...props}>
             <i className="material-icons">{icon}</i>
         </Button>
     );
