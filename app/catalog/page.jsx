6199add8d10b117b7e17c540712bf9e087ff1015
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-19 16:13:25 -0400

    updated styles

--- app/catalog/page.jsx
+++ app/catalog/page.jsx
@@ -1,9 +1,13 @@
+"use client"
+import { useState } from "react";
 import Banner from "../assets/components/banner";
 import {Display} from "../assets/components/typography";
 import QueryHandler from "../assets/components/query-handler";
 import Filters from "../assets/components/filters";
 // import Search from "../assets/components/search";
 import Listings from "../assets/components/listings";
+import "../assets/styles/pages/catalog.scss";
+import IconButton from "../assets/components/icon-button";
 
 export default function Catalog({searchParams}) {
     return (
@@ -12,13 +16,13 @@ export default function Catalog({searchParams}) {
                 <Display size="medium">Catalog</Display>
             </Banner>
             <QueryHandler>
-                <div>
+                <section className="catalog">
                     <Filters />
-                    <div>
+                    <div className="listings">
                         {/* <Search /> */}
                         <Listings searchParams={searchParams} />
                     </div>
-                </div>
+                </section>
             </QueryHandler>
         </main>
     )
