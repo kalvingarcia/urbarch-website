270f701807469415b182046beb6b5d094f9fe9f0
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-23 16:08:02 -0400

    added search bar to the catalog

--- app/catalog/page.jsx
+++ app/catalog/page.jsx
@@ -1,15 +1,12 @@
-"use client"
-import { useState } from "react";
 import Banner from "../assets/components/banner";
 import {Display} from "../assets/components/typography";
 import QueryHandler from "../assets/components/query-handler";
 import Filters from "../assets/components/filters";
-// import Search from "../assets/components/search";
+import Search from "../assets/components/search";
 import Listings from "../assets/components/listings";
 import "../assets/styles/pages/catalog.scss";
-import IconButton from "../assets/components/icon-button";
 
-export default function Catalog({searchParams}) {
+export default function Catalog() {
     return (
         <main>
             <Banner src="catalog.jpg">
@@ -19,8 +16,8 @@ export default function Catalog({searchParams}) {
                 <section className="catalog">
                     <Filters />
                     <div className="listings">
-                        {/* <Search /> */}
-                        <Listings searchParams={searchParams} />
+                        <Search />
+                        <Listings />
                     </div>
                 </section>
             </QueryHandler>
