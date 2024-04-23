f274ae7ba50025b9da978c3f363cb863ea3c1982
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-23 16:01:59 -0400

    modified filter to function when just filter, search, or both are applied

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -29,35 +29,70 @@ export async function GET(request) {
                     filters[parameter] = [];
                 filters[parameter].push(id);
             }
-    
-        result = await Database`
-            WITH search_filtered AS (
-                SELECT DISTINCT product_variations.listing_id AS id, product_variations.price AS price
-                FROM product_listing INNER JOIN product_variations ON product_variations.listing_id = product_listing.id
-                WHERE product_listing.index @@ plainto_tsquery('english', '${search}') OR product_variations.index @@ plainto_tsquery('english', '${search}')
-                UNION
-                SELECT DISTINCT product_variations.listing_id AS id, product_variations.price AS price
-                FROM product_listing INNER JOIN product_variations ON product_variations.listing_id = product_listing.id
-                WHERE product_listing.name LIKE '${search}%' OR product_variations.subname LIKE '${search}%'
-            ), tag_filtered AS (
-                ${Object.entries(filters).map(([_, value]) => {
-                    return `
-                        SELECT DISTINCT listing_id AS id FROM product_variation__tag WHERE tag_id = ANY ARRAY([${
-                            value.map(id => `'${id}'::INT`).join(",")
-                        }])
-                    `
-                }).join(" INTERSECT ")}
-            ), categories AS (
-                /* Create a table with the tag name and listing id */
-                SELECT DISTINCT listing_id AS id, tag.name AS category
-                FROM tag INNER JOIN tag_categories ON tag.category_id = tag_categories.id  /* First we combine the tag and tag category information */
-                    INNER JOIN product_variation__tag ON product_variation__tag.tag_id = tag.id /* Then we combine the tags specific to the variations we have */
-                WHERE tag_categories.name = 'Category' AND listing_id IN (SELECT listing_id FROM variations)
-            )
-            SELECT id, name, category, MIN(price) AS price
-            FROM search_filtered INNER JOIN tag_filtered USING(id) INNER JOIN categories USING(id)
-            GROUP BY id;
-        `
+
+        if(search !== "" && Object.entries(filters).length === 0)
+            result = await Database`
+                WITH search_filtered AS (
+                    SELECT DISTINCT product_variations.listing_id AS id, product_listing.name as name, product_variations.price AS price
+                    FROM product_listing INNER JOIN product_variations ON product_variations.listing_id = product_listing.id
+                    WHERE product_listing.index @@ to_tsquery(CONCAT(CAST(${search} AS TEXT), ':*')) OR product_variations.index @@ to_tsquery(CONCAT(CAST(${search} AS TEXT), ':*'))
+                ), categories AS (
+                    /* Create a table with the tag name and listing id */
+                    SELECT DISTINCT listing_id AS id, tag.name AS category
+                    FROM tag INNER JOIN tag_categories ON tag.category_id = tag_categories.id  /* First we combine the tag and tag category information */
+                        INNER JOIN product_variation__tag ON product_variation__tag.tag_id = tag.id /* Then we combine the tags specific to the variations we have */
+                    WHERE tag_categories.name = 'Class' AND listing_id IN (SELECT id FROM search_filtered)
+                )
+                SELECT id, name, category, MIN(price) AS price
+                FROM search_filtered INNER JOIN categories USING(id)
+                GROUP BY id, name, category;
+            `
+        else if(search === "" && Object.entries(filters).length !== 0)
+            result = await Database`
+                WITH tag_filtered AS (
+                    ${Object.entries(filters).map(([_, value]) => {
+                        return `
+                            SELECT DISTINCT listing_id AS id FROM product_variation__tag WHERE tag_id = ANY ARRAY([${
+                                value.map(id => `'${id}'::INT`).join(",")
+                            }])
+                        `
+                    }).join(" INTERSECT ")}
+                ), categories AS (
+                    /* Create a table with the tag name and listing id */
+                    SELECT DISTINCT listing_id AS id, tag.name AS category
+                    FROM tag INNER JOIN tag_categories ON tag.category_id = tag_categories.id  /* First we combine the tag and tag category information */
+                        INNER JOIN product_variation__tag ON product_variation__tag.tag_id = tag.id /* Then we combine the tags specific to the variations we have */
+                    WHERE tag_categories.name = 'Category' AND listing_id IN (SELECT listing_id FROM variations)
+                )
+                SELECT id, name, category, MIN(price) AS price
+                FROM tag_filtered INNER JOIN categories USING(id)
+                GROUP BY id;
+            `
+        else 
+            result = await Database`
+                WITH search_filtered AS (
+                    SELECT DISTINCT product_variations.listing_id AS id, product_listing.name as name, product_variations.price AS price
+                    FROM product_listing INNER JOIN product_variations ON product_variations.listing_id = product_listing.id
+                    WHERE product_listing.index @@ to_tsquery(CONCAT(CAST(${search} AS TEXT), ':*')) OR product_variations.index @@ to_tsquery(CONCAT(CAST(${search} AS TEXT), ':*'))
+                ), tag_filtered AS (
+                    ${Object.entries(filters).map(([_, value]) => {
+                        return `
+                            SELECT DISTINCT listing_id AS id FROM product_variation__tag WHERE tag_id = ANY ARRAY([${
+                                value.map(id => `'${id}'::INT`).join(",")
+                            }])
+                        `
+                    }).join(" INTERSECT ")}
+                ), categories AS (
+                    /* Create a table with the tag name and listing id */
+                    SELECT DISTINCT listing_id AS id, tag.name AS category
+                    FROM tag INNER JOIN tag_categories ON tag.category_id = tag_categories.id  /* First we combine the tag and tag category information */
+                        INNER JOIN product_variation__tag ON product_variation__tag.tag_id = tag.id /* Then we combine the tags specific to the variations we have */
+                    WHERE tag_categories.name = 'Category' AND listing_id IN (SELECT listing_id FROM variations)
+                )
+                SELECT id, name, category, MIN(price) AS price
+                FROM search_filtered INNER JOIN tag_filtered USING(id) INNER JOIN categories USING(id)
+                GROUP BY id;
+            `
     }
 
     return new Response(JSON.stringify(result), {
