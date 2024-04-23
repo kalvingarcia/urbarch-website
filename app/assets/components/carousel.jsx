f9eea958c6fa2dc16ba1aae21b33ff9c38a30b58
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-23 09:37:49 -0400

    finished implementing pagination

--- app/assets/components/carousel.jsx
+++ app/assets/components/carousel.jsx
@@ -1,7 +1,9 @@
 "use client"
-import React, {useState, useEffect, useCallback, createRef, forwardRef, memo} from 'react';
+import React, {useState, useCallback} from 'react';
 import Button from './button';
 import IconButton from './icon-button';
+import useWindowSize from '../hooks/window';
+import '../styles/components/carousel.scss';
 
 const DEFAULT_PER_PAGE = 15;
 
@@ -10,6 +12,12 @@ function Slide({children}) {
         <div className='slide'>
             {children}
         </div>
+    );
+}
+
+function PaginationGap() {
+    return (
+        <span className="page-button">...</span>
     );
 }
 
@@ -22,17 +30,43 @@ function PageButton({number, ...props}) {
 }
 
 function Pagination({count, activeSlide, changeSlide}) {
+    const {width} = useWindowSize();
     const generatePageButtons = useCallback(count => {
+        var buttonCount = 5;
+        if(width <= 600)
+            buttonCount = 3
+
         const pageButtons = [];
-        for(let i = 0; i < count; i++)
-            pageButtons.push(<PageButton key={i} role="primary" style={activeSlide === i? "filled" : "outlined"} onPress={() => changeSlide(i)} number={i + 1} />);
+
+        if(count <= buttonCount) {
+            for(let i = 0; i < count; i++)
+                pageButtons.push(<PageButton key={i} role="primary" style={activeSlide === i? "filled" : "outlined"} onPress={() => changeSlide(i)} number={i + 1} />);
+        } else if(activeSlide < buttonCount / 2) {
+            for(let i = 0; i < buttonCount; i++)
+                pageButtons.push(<PageButton key={i} role="primary" style={activeSlide === i? "filled" : "outlined"} onPress={() => changeSlide(i)} number={i + 1} />);
+            pageButtons.push(<PaginationGap key="gap_right" />);
+            pageButtons.push(<PageButton key={count - 1} role="primary" style={activeSlide === count - 1? "filled" : "outlined"} onPress={() => changeSlide(count - 1)} number={count} />);
+        } else if(activeSlide > count - buttonCount / 2 - 1) {
+            pageButtons.push(<PageButton key={0} role="primary" style={activeSlide === 0? "filled" : "outlined"} onPress={() => changeSlide(0)} number="1" />);
+            pageButtons.push(<PaginationGap key="gap_left" />);
+            for(let i = count - buttonCount; i < count; i++)
+                pageButtons.push(<PageButton key={i} role="primary" style={activeSlide === i? "filled" : "outlined"} onPress={() => changeSlide(i)} number={i + 1} />);
+        } else {
+            pageButtons.push(<PageButton key={0} role="primary" style={activeSlide === 0? "filled" : "outlined"} onPress={() => changeSlide(0)} number="1" />);
+            pageButtons.push(<PaginationGap key="gap_left" />);
+            for(let i = activeSlide - (buttonCount - 3) / 2; i < activeSlide + (buttonCount -3) / 2 + 1; i++)
+                pageButtons.push(<PageButton key={i} role="primary" style={activeSlide === i? "filled" : "outlined"} onPress={() => changeSlide(i)} number={i + 1} />);
+            pageButtons.push(<PaginationGap key="gap_right" />);
+            pageButtons.push(<PageButton key={count - 1} role="primary" style={activeSlide === count - 1? "filled" : "outlined"} onPress={() => changeSlide(count - 1)} number={count} />);
+        }
+
         return pageButtons;
-    }, [activeSlide]);
+    }, [activeSlide, width]);
 
     return (
         <div className='pagination'>
             <IconButton className="left" role="primary" style="tonal" icon="navigate_before" onPress={() => changeSlide(activeSlide - 1)} />
-            <div>
+            <div className='button-list'>
                 {generatePageButtons(count)}
             </div>
             <IconButton className="left" role="primary" style="tonal" icon="navigate_next" onPress={() => changeSlide(activeSlide + 1)} />
@@ -68,7 +102,7 @@ export default function Carousel({children}) {
             setDirection("left");
         console.log(activeSlide, nextSlide);
         setActiveSlide(nextSlide);
-    }, [direction]);
+    }, [activeSlide, direction]);
 
     return (
         <div className='carousel'>
