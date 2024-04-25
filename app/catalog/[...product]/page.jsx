215263e28928e164e8b80fae40d641f6e2daee50
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-25 16:49:46 -0400

    begun implementing dynamic image loading for products

--- app/catalog/[...product]/page.jsx
+++ app/catalog/[...product]/page.jsx
@@ -1,12 +1,65 @@
 import {redirect} from 'next/navigation';
+import Spotlight from '@/app/assets/components/spotlight';
+import Image from 'next/image';
+import {Heading, Title} from '@/app/assets/components/typography';
+import Button from '@/app/assets/components/button';
 
-export default function Product({params: {product: [id, extension, ...rest]}}) {
+export default async function Product({params: {product: [id, extension, ...rest]}}) {
     if(rest.length > 0)
         redirect(`/catalog/${id}/${extension}`);
+    if(extension === "DEFAULT")
+        redirect(`/catalog/${id}`);
 
+    if(!extension)
+        extension = "DEFAULT";
+
+    let count = 0;
+    const images = [];
+    while(true) {
+        const image = {
+            name: `${count}.jpg`,
+            src: (await import(`../../assets/images/products/${id}/${extension}/${count}.jpg`).catch(() => undefined))?.default
+        };
+        if(image.src === undefined)
+            break;
+        images.push(image);
+        count++;
+    }
+    console.log(images);
     
-
-    return(
-        <div>{`${id} ${extension}`}</div>
-    )
+    return (
+        <main style={{height: "2000px"}}>
+            <Spotlight>
+            {images.map(image => (
+                <Image key={image.name} src={image.src} />
+            ))}
+            </Spotlight>
+        </main>
+    );
 }
+
+/* 
+<section>
+    <Slideshow>
+        {images.map(image => (
+            <Image />
+        ))}
+    </Slideshow>
+    <div>
+        <Title>{}</Title>
+        <span>{}</span>
+        <Heading>{}</Heading>
+        <p>{}</p>
+        <Button>Product Details</Button>
+        <div>
+            {options.map(option => (
+                <DropdownMenu key={option.name} name={option.name}>
+                    {option.choices.map(choice => <Option></Option>)}
+                </DropdownMenu>
+            ))}
+        </div>
+    </div>
+</section>
+<RelatedProducts id={id} extension={extension} />
+<CustomGallery id={id} extension={extension} /> 
+*/
