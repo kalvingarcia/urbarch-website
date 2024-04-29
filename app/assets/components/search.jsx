1f270c40d75d29d6edb0c80a8fcfd1ab1cb853d5
Kalvin Garcia <kalvigarcia@gmail.com>
2024-04-29 16:01:11 -0400

    added a ripple to the search bar

--- app/assets/components/search.jsx
+++ app/assets/components/search.jsx
@@ -2,14 +2,16 @@
 import {useContext, useState} from 'react';
 import {QueryContext} from './query-handler';
 import IconButton from "./icon-button";
+import useRippleEffect from '../hooks/ripple';
 import '../styles/components/search.scss';
 
 export default function Search() {
+    const [rippleExpand, rippleFade] = useRippleEffect();
     const {getSearch, setSearch, applyRoute} = useContext(QueryContext);
     const [text, setText] = useState(getSearch);
 
     return (
-        <div className='search'>
+        <div className='search' onMouseDown={rippleExpand} onMouseUp={rippleFade} onClick={event => event.target.getElementsByClassName('textbox')[0]?.focus()}>
             <input className="textbox" name="search" value={text} onChange={event => setText(event.target.value) || setSearch(event.target.value)} placeholder='Search (e.g. Product ID, Name, Style, etc.)' />
             <div className="divider" />
             <IconButton role="secondary" style="text" icon="search" onPress={applyRoute} />
