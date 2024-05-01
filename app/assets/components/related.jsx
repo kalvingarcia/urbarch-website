af64233d3bc4b1efcc8cbeefe91a9a6740b79bca
Kalvin Garcia <kalvigarcia@gmail.com>
2024-05-01 16:05:59 -0400

    added style to related that distinguishes it from custom gallery when they're together on the page

--- app/assets/components/related.jsx
+++ app/assets/components/related.jsx
@@ -1,9 +1,9 @@
 import {Heading} from "./typography";
 import '../styles/components/related.scss';
 
-export default function Related({children}) {
+export default function Related({alone = false, children}) {
     return (
-        <section className="related">
+        <section className={["related", alone? "alone" : ""].join(" ")}>
             <Heading>Related Products</Heading>
             <div className="cards">
                 {children}
