0db3b54371259f01aaca9b2ef2899305498bf984
Kalvin Garcia <kalvigarcia@gmail.com>
2024-05-08 14:59:21 -0400

    added the overview popup

--- app/assets/components/product-data.jsx
+++ app/assets/components/product-data.jsx
@@ -1,15 +1,18 @@
 "use client"
-import {Heading, Subtitle, Title} from './typography';
+import Image from 'next/image';
+import {useState} from 'react';
+import {Heading, Subheading, Subtitle, Title} from './typography';
 import Button from './button';
 import DropdownMenu from './dropdown-menu';
 import FinishesMenu from './finishes-menu';
 import usePriceChange from '../hooks/price-change';
 import '../styles/components/metadata.scss';
 
-export default function ProductData({product, extension}) {
+export default function ProductData({product, extension, drawing}) {
     const variation = product.variations.find(variation => variation.extension === extension);
     const [price, onPriceChange] = usePriceChange(variation.price);
 
+    const [open, setOpen] = useState(false);
     return (
         <div className='metadata'>
             <div className='general'>
@@ -20,7 +23,7 @@ export default function ProductData({product, extension}) {
                 <span className='id'>{product.id}{extension !== "DEFAULT"? "-" + extension : ""}</span>
                 <span className='price'>${price}.00</span>
                 <p className='description'>{product.description}</p>
-                <Button role="primary" style="filled">Product Details</Button>
+                <Button role="primary" style="filled" onPress={() => setOpen(true)}>Product Details</Button>
             </div>
             <div className='options'>
                 <Heading>Options</Heading>
@@ -28,6 +31,32 @@ export default function ProductData({product, extension}) {
                 {Object.entries(variation.overview.options).map(([name, choices]) => (
                     <DropdownMenu key={name} name={name} choices={choices} onChange={onPriceChange} />
                 ))}
+            </div>
+            <div className={['modal', open? 'open' : ''].join(" ")}>
+                <div className='scrim' onClick={() => setOpen(false)} />
+                <div className='overview'>
+                    <Image src={drawing} alt="" />
+                    <Heading>Product Overview</Heading>
+                    {variation.replacements?
+                        <div className='replacements'>
+                            <Subheading>Replacements</Subheading>
+                            <div className='list'>
+                                {variation.replacements.map(replacement =>(
+                                    <a className="replacement">
+                                        <span>{replacement.name}</span>
+                                        <span>{replacement.id}</span>
+                                        <span>${replacement.price}</span>
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                        :
+                        ""
+                    }
+                    <div className='notes'>
+                        <Subheading>Notes</Subheading>
+                    </div> 
+                </div>
             </div>
         </div>
     );
