87ee435b964f7cc0ce3ba9cfce8f36c76d4bc1c2
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-07 16:03:16 -0400

    added a modal component to abstract modal functionality

---
+++ app/assets/components/modal.jsx
@@ -0,0 +1,20 @@
+import {createPortal} from "react-dom";
+import IconButton from "./icon-button";
+import '../styles/components/modal.scss';
+
+export default function Modal({open, setOpen, children}) {
+    return (open?
+        createPortal(
+            <div className="modal">
+                <div className="scrim" onClick={() => setOpen(false)} />
+                <div className="pop-up">
+                    <IconButton className="close" icon="close" onPress={() => setOpen(false)} />
+                    {children}
+                </div>
+            </div>,
+            document.getElementsByClassName("root")[0]
+        )
+        :
+        ""
+    );
+}
