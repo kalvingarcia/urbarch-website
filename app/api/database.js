7dbce68add857a64ca567c553086d027ae8de176
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-10 10:48:40 -0400

    preparing for first test deployment

--- app/api/database.js
+++ app/api/database.js
@@ -1,6 +1,17 @@
 import postgres from 'postgres';
 import environment from '../assets/sessions/database-env.json'
 
-const Database = postgres(environment);
+const Database = postgres(environment? 
+    environment 
+    :
+    {
+        database: process.env.DBNAME, //"productdb",
+        username: process.env.DBUSER, //"dashboard",
+        password: process.env.DBPASS, //"KQrIInGFvRr6Vqm5KF0_Zw",
+        host: process.env.DBHOST, //"urban-website-737.jxf.cockroachlabs.cloud",
+        port: process.env.DBPORT, //26257,
+        ssl: process.env.DBMODE, //true
+    }
+);
 
 export default Database;
