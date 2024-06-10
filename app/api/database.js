d997d2c170642988aba1a20c9cba729c391f378d
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-10 11:23:28 -0400

    creating error handling for when the session file doesn't exist

--- app/api/database.js
+++ app/api/database.js
@@ -1,17 +1,21 @@
 import postgres from 'postgres';
-import environment from '../assets/sessions/database-env.json'
 
-const Database = postgres(environment? 
-    environment 
-    :
-    {
+const SESSIONS_FILENAME = 'database-env.json';
+
+const Database = await import(`../assets/sessions/${SESSIONS_FILENAME}`).then(response => {
+    console.log(response);
+    return postgres(response);
+}).catch(err => {
+    console.error("Error loading session module: " + err);
+    console.error("Assuming the build is on a server.");
+    return postgres({
         database: process.env.DBNAME,
         username: process.env.DBUSER,
         password: process.env.DBPASS,
         host: process.env.DBHOST,
         port: process.env.DBPORT,
         ssl: process.env.DBMODE
-    }
-);
+    });
+});
 
 export default Database;
