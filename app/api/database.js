bd0e35c30e17914a9b733f0fbf56cbcb7981175e
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-10 11:43:38 -0400

    attempting to fix issues with deployment

--- app/api/database.js
+++ app/api/database.js
@@ -1,21 +1,17 @@
 import postgres from 'postgres';
 
-const SESSIONS_FILENAME = 'database-env.json';
-
-const Database = await import(`../assets/sessions/${SESSIONS_FILENAME}`).then(response => {
-    console.log(response);
-    return postgres(response);
-}).catch(err => {
-    console.error("Error loading session module: " + err);
-    console.error("Assuming the build is on a server.");
-    return postgres({
+let Database;
+if(process.env.NODE_ENV === "development")
+    Database = await import('../assets/sessions/database-env.json').then(response => {
+        return postgres(response);
+    });
+else
+    Database = postgres({
         database: process.env.DBNAME,
         username: process.env.DBUSER,
         password: process.env.DBPASS,
         host: process.env.DBHOST,
         port: process.env.DBPORT,
         ssl: process.env.DBMODE
-    });
-});
-
+    }); 
 export default Database;
--- app/assets/sessions/README.txt
+++
@@ -1,2 +0,0 @@
-This directory is used to hold the session information while I'm developing.
-Things that would normally go into environment variables.
