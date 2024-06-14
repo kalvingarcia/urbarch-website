1d80f00125b2c496b127fad3695b727e4af2ac62
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-14 15:05:45 -0400

    fixed query to only provide category and class tag instead of all

--- app/api/product/cutsheet/route.js
+++ app/api/product/cutsheet/route.js
@@ -17,15 +17,26 @@ export async function GET(request) {
     const productData = (await Database`
         WITH variations AS (
             SELECT listing_id AS id, extension, subname, price, overview, (
-                SELECT COALESCE(json_agg(json_build_object(
+                SELECT json_build_object(
                     'id', tag.id,
                     'name', tag.name,
                     'category_id', tag_category.name,
                     'listing_id', product_variation__tag.listing_id
-                )), '[]') FROM tag INNER JOIN tag_category ON tag.category_id = tag_category.id
+                ) FROM tag INNER JOIN tag_category ON tag.category_id = tag_category.id
                     INNER JOIN product_variation__tag ON product_variation__tag.tag_id = tag.id
                 WHERE product_variation__tag.listing_id = ${id} AND product_variation__tag.variation_extension = extension
-            ) AS tags, (
+                    AND tag_category.name = 'Class'
+            ) AS class, (
+                SELECT json_build_object(
+                    'id', tag.id,
+                    'name', tag.name,
+                    'category_id', tag_category.name,
+                    'listing_id', product_variation__tag.listing_id
+                ) FROM tag INNER JOIN tag_category ON tag.category_id = tag_category.id
+                    INNER JOIN product_variation__tag ON product_variation__tag.tag_id = tag.id
+                WHERE product_variation__tag.listing_id = ${id} AND product_variation__tag.variation_extension = extension
+                    AND tag_category.name = 'Category'
+            ) AS category, (
                 SELECT DISTINCT COALESCE(json_agg(json_build_object(
                     'id', product_listing.id,
                     'extension', product_variation.extension,
@@ -38,7 +49,7 @@ export async function GET(request) {
             ) AS replacements
             FROM product_variation WHERE listing_id = ${id} AND display = TRUE
         )
-        SELECT id, extension, name, subname, description, price, overview, tags, replacements
+        SELECT id, extension, name, subname, description, price, overview, class, category, replacements
         FROM product_listing INNER JOIN variations USING(id)
         WHERE id = ${id} AND extension = ${extension};
     `)[0];
