c9cc59d742b1eca6316656aaf41c58d76ad18d7e
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-17 13:13:20 -0400

    making small adjustments for efficiency

--- app/assets/components/finishes-menu.jsx
+++ app/assets/components/finishes-menu.jsx
@@ -1,19 +1,20 @@
 import DropdownMenu from "./dropdown-menu";
 
+const finishes = {
+    "PB": "Polished Brass",
+    "GP": "Green Patina",
+    "BP": "Brown Patina",
+    "AB": "Antique Brass",
+    "STBL": "Statuary Black",
+    "STBR": "Statuary Brown",
+    "PN": "Polished Nickel",
+    "SN": "Satin Nickel",
+    "PC": "Polished Chrome",
+    "LP": "Light Pewter",
+    "BN": "Black Nickel"
+};
+
 export default function FinishesMenu({choices, ...props}) {
-    const finishes = {
-        "PB": "Polished Brass",
-        "GP": "Green Patina",
-        "BP": "Brown Patina",
-        "AB": "Antique Brass",
-        "STBL": "Statuary Black",
-        "STBR": "Statuary Brown",
-        "PN": "Polished Nickel",
-        "SN": "Satin Nickel",
-        "PC": "Polished Chrome",
-        "LP": "Light Pewter",
-        "BN": "Black Nickel"
-    };
     choices = choices.map((choice) => ({
         value: choice.finish,
         display: finishes[choice.finish],
