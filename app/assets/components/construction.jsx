e33697e321d4150cb39237849227c1c345234f5e
Kalvin Garcia <kalvigarcia@gmail.com>
2024-11-21 10:04:11 -0500

    Changed construction message

--- app/assets/components/construction.jsx
+++ app/assets/components/construction.jsx
@@ -10,10 +10,10 @@ export default function Construction() {
             <Title>Something New is Under Construction!</Title>
             <div className="divider" />
             <p>
-                Currently our website is in open beta, which means we're still in the process of adding
-                in-depth product information, our custom products, and other new website features! While
-                we complete this process, please feel free to give us insight on your experience and how
-                we can improve.
+                Currently our website is in the alpha stages, which means we're still in the process of adding
+                product images, updating product information, and preparing to launch the website in beta for 
+                the public! While we complete this process, please feel free to give us insight on your experience
+                and how we can improve.
             </p>
             <a href="https://forms.gle/c6KUMZ9wDvTvRyDKA" target='_blank'>
                 <Button>Send Feedback</Button>
@@ -21,3 +21,10 @@ export default function Construction() {
         </div>
     );
 }
+
+/*
+Currently our website is in the beta stages, which means we're still in the process of adding
+in-depth product information, our custom products, and other new website features! While
+we complete this process, please feel free to give us insight on your experience and how
+we can improve.
+*/
